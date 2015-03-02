132d93c26bd22396bfb13c5d30e12a2b13206f55
Anton Weber <ant@antweb.me>
2015-03-02 11:32:18 +0100

    Task 2: enter_critical and exit_critical

--- lab2/src/test_critical.c
+++ lab2/src/test_critical.c
@@ -24,9 +24,11 @@ increase(int thread, int iterations, volatile int *data)
                  * exit_critical(thread) to define a critical section
                  * around the code that needs to execute atomically.
                  */
+                enter_critical(thread);
                 a = *data;
                 a++;
                 *data = a;
+                exit_critical(thread);
         }
 }
 
@@ -43,9 +45,11 @@ decrease(int thread, int iterations, volatile int *data)
                  * exit_critical(thread) to define a critical section
                  * around the code that needs to execute atomically.
                  */
+                enter_critical(thread);
                 a = *data;
                 a--;
                 *data = a;
+                exit_critical(thread);
         }
 }
 
